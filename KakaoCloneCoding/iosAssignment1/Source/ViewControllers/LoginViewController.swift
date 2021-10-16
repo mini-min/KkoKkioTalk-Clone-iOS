f3c2f7f0538e284adf2bd26c6724d499b28ff6aa
MinjaeLee <2alswo7@khu.ac.kr>
2021-10-16 20:16:55 +0900

    🎨컬러 에셋 추가

--- KakaoCloneCoding/iosAssignment1/Source/ViewControllers/LoginViewController.swift
+++ KakaoCloneCoding/iosAssignment1/Source/ViewControllers/LoginViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlet Properties
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBAction Properties
+    @IBAction func PresentToMainView(_ sender: Any) {
     }
-    */
-
+    @IBAction func pushToSignUpView(_ sender: Any) {
+    }
+    
 }
