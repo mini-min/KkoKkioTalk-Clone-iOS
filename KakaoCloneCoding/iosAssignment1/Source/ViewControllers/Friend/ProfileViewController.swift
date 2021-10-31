31e1959e4b968f3e0ffd4307450eb9bbcbf31c20
MinjaeLee <2alswo7@khu.ac.kr>
2021-10-31 14:54:07 +0900

    ✨[#3] - 스와이프로 dismiss 추가

--- KakaoCloneCoding/iosAssignment1/Source/ViewControllers/Friend/ProfileViewController.swift
+++ KakaoCloneCoding/iosAssignment1/Source/ViewControllers/Friend/ProfileViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    var initialTouchPoint = CGPoint(x: 0, y: 0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setPanGesture()
     }
     
     @IBAction func closeButtonClicked(_ sender: Any) {
@@ -26,4 +28,28 @@ class ProfileViewController: UIViewController {
     @IBAction func kakaoStoryButtonClicked(_ sender: Any) {
     }
     
+    func setPanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureDismiss(_:)))
+        self.view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func panGestureDismiss(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view.window)
+        
+        if sender.state == .began {
+            initialTouchPoint = touchPoint
+        } else if sender.state == .changed {
+            if touchPoint.y - initialTouchPoint.y > 0 {
+                self.view.frame = CGRect(x: 0, y: touchPoint.y - initialTouchPoint.y, width: self.view.frame.width, height: self.view.frame.height)
+            }
+        } else if sender.state == .ended || sender.state == .cancelled {
+            if touchPoint.y - initialTouchPoint.y > 200 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+                }
+            }
+        }
+    }
 }
